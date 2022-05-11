2ee488c85dc29c9105ef028ca1cf14ff18e62540
elknhns <elknhns@gmail.com>
2022-05-11 23:49:25 +0700

    Replace hard codes to dynamic programming

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import React from "react"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Card from "./components/Card"
+import data from "./data"
 export default function App() {
+    const cards = data.map(item => 
+        <Card
+            key={item.id}
+            {...item}
+        />
+    )
+    
     return (
         <div>
             <Navbar />
             <Hero />
-            <Card />
+            <section className="card__list">
+                {cards}
+            </section>
         </div>
     )
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,30 +1,38 @@
 import React from "react"
-import cardImage from "../images/katie-zaferes.png"
 import starIcon from "../images/star.png"
 import "../index.css"
 
-export default function Card() {
+export default function Card(props) {
+    
+    console.log('props', JSON.stringify(props, null, 2))
+    const makeBadgeText = (openSpots, location) => {
+        if (openSpots === 0) return "SOLD OUT"
+        if (location === "Online") return "ONLINE"
+    }
+    
+    const badgeText = makeBadgeText(props.openSpots, props.location)
+
     return (
+        
         <section className="card">
-            <div className="card__image">
-                <div className="card__badge">SOLD OUT</div>
-                <img src={cardImage} alt="Katie Zaferes" />
-            </div>
+            {badgeText && <div className="card__badge">{badgeText}</div>}
+            <img src={require(`../images/${props.coverImg}`)} alt={props.img} />
 
             <div className="card__caption">
                 <div className="card__rating">
                     <img src={starIcon} alt="star" />
-                    <span className="card__score">5.0</span>
-                    <span>(6)</span>
+                    <span className="card__score">{props.stats.rating}</span>
+                    <span>({props.stats.reviewCount})</span>
                     <span className="dot"></span>
-                    <span>USA</span>
+                    <span>{props.location}</span>
                 </div>
 
-                <p>Life lessons with Katie Zaferes</p>
+                <p>{props.title}</p>
                 <p>
-                    <span className="card__price">From $136</span> / person
+                    <span className="card__price">From ${props.price}</span> / person
                 </p>
             </div>
         </section>
+
     )
 }
